4480384d169717e6366863b01adc3aaf9eb88fba
dabin <dabin@dabin-PC>
2015-09-22 16:04:12 +0800

    add thirdactivity 111 / 222

--- src/com/example/githubdemo/SecondActivity.java
+++ src/com/example/githubdemo/SecondActivity.java
@@ -30,6 +30,8 @@ public class SecondActivity extends Activity {
 		System.out.println("1111111111111111111");
 		
 		System.out.println("22222222222222222222");
+		System.out.println("33333333333");
+		System.out.println("44444444444");
 	}
 
 
---
+++ src/com/example/githubdemo/ThirdActivity.java
@@ -0,0 +1,20 @@
+package com.example.githubdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ThirdActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		initView();
+	}
+
+	private void initView() {
+		System.out.println("1111111111111");
+		System.out.println("22222222222222");
+		
+	}
+}
