b0bed07ca7cbf8ecf8b2855156307e210b82bb2d
dabin <dabin@dabin-PC>
2015-09-22 15:43:26 +0800

    add a toast

--- src/com/example/githubdemo/SecondActivity.java
+++ src/com/example/githubdemo/SecondActivity.java
@@ -2,17 +2,27 @@ package com.example.githubdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SecondActivity extends Activity {
+	
+	private Activity mActivity;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_second);
+		mActivity = this;
+		
+		initView();
 		
 		
-		
+	}
+
+	private void initView() {
+		Toast.makeText(mActivity, "hello dabin", 0).show();
 		
 	}
 
