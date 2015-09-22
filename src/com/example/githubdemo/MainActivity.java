95e47336270aefe40cdef2af521b04f859509c15
dabin <dabin@dabin-PC>
2015-09-22 15:33:15 +0800

    create ActivitySecond

--- src/com/example/githubdemo/MainActivity.java
+++ src/com/example/githubdemo/MainActivity.java
@@ -1,26 +1,50 @@
 package com.example.githubdemo;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-import android.widget.Toast;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
-    @Override
+    private Button mButton;
+    private Activity mActivity;
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        Toast.makeText(getApplicationContext(), "hello world", 0).show();
+        initView();
+        setListener();
+        mActivity = this;
     }
 
+	private void setListener() {
+		mButton.setOnClickListener(this);
+		
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	private void initView() {
+		mButton = (Button) findViewById(R.id.button);
+		
+	}
+
+	
+
+	@Override
+	public void onClick(View view) {
+		switch(view.getId()){
+		case R.id.button:
+			startActivity(new Intent(mActivity,SecondActivity.class));
+			break;
+		}
+		
+	}
+
+
+ 
     
 }
